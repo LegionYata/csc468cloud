6c1e6a7b604fc1b367daaa40abf6452e34328b6f
Logan Yates <115040016+LegionYata@users.noreply.github.com>
2026-03-19 19:39:29 -0400

    Update server.js

--- shark_app/node_server/server.js
+++ shark_app/node_server/server.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const app = express();
 
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/sharkinfo';
+
+mongoose.connect(MONGO_URL)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.listen(8080, () => {
+    console.log('Server running on http://localhost:8080');
+});
